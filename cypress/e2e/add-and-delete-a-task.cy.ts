459ef0358713fa477cb9141c7a408712565b99e5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-05 12:32:46 -0400

    refactor: move cypress selectors to a separate file

--- cypress/e2e/add-and-delete-a-task.cy.ts
+++ cypress/e2e/add-and-delete-a-task.cy.ts
@@ -1,41 +1,43 @@
+import { selectors } from "../support/selectors";
+
 it("adds and deletes a task", () => {
   // using the data-cy selectors from the src/ folder
   // visit the app
   cy.visit("/");
 
   // confirm the list-view is visible
-  cy.get('[data-cy="list-view"]').should("be.visible");
+  cy.get(selectors.ListView.container).should("be.visible");
 
   // click the create task button
-  cy.get('[data-cy="create-task-button"]').click();
+  cy.get(selectors.CreateTaskButton.button).click();
 
   // confirm the task form is visible
-  cy.get('[data-cy="task-form"]').should("be.visible");
+  cy.get(selectors.TaskForm.container).should("be.visible");
 
   // within the form:
-  cy.get('[data-cy="task-form"]').within(() => {
+  cy.get(selectors.TaskForm.container).within(() => {
     // enter a title
-    cy.get('input[name="title"]').type("Test Task");
+    cy.get(selectors.TaskForm.titleInput).type("Test Task");
 
     // enter a description
-    cy.get('textarea[name="description"]').type("This is a test task description");
+    cy.get(selectors.TaskForm.descriptionInput).type("This is a test task description");
 
     // select a status from the dropdown
-    cy.get('[data-cy="status-select"]').select("In Progress");
+    cy.get(selectors.TaskForm.statusSelect).select("In Progress");
 
     // select a priority from the dropdown
-    cy.get('[data-cy="priority-select"]').select("High");
+    cy.get(selectors.TaskForm.prioritySelect).select("High");
 
     // click the create task button
-    cy.get('[data-cy="submit-task-button"]').click();
+    cy.get(selectors.TaskForm.submitButton).click();
   });
 
   // confirm the task is visible in the list-view
-  cy.get('[data-cy="task-row"]').should("contain", "Test Task");
+  cy.get(selectors.ListView.taskRow).should("contain", "Test Task");
 
   // click the delete button
-  cy.get('[data-cy="delete-task-button"]').first().click();
+  cy.get(selectors.DeleteTaskButton.button).first().click();
 
   // confirm the task is no longer visible in the list-view
-  cy.get('[data-cy="task-row"]').should("not.exist");
+  cy.get(selectors.ListView.taskRow).should("not.exist");
 });
