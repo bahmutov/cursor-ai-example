c55d9a46d9e5802048b8b845f6db90dc72737b2d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-08 11:11:04 -0400

    start spec

---
+++ cypress/e2e/task-in-list-and-kanban.cy.ts
@@ -0,0 +1,9 @@
+it("shows the task in the list view and kanban view", () => {
+  // visit the app
+  // verify the List view is visible
+  // create a task
+  // confirm the task is shown in the List view
+  // click on the "Kanban" button
+  // confirm the Kanban view is visible
+  // confirm the task is shown in the Kanban view
+});
