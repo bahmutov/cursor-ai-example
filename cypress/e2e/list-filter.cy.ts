4b0ceeb2783f4406d63654bedc2316606ab7131b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-10 08:22:03 -0400

    remove extra comments

--- cypress/e2e/list-filter.cy.ts
+++ cypress/e2e/list-filter.cy.ts
@@ -1,9 +1,3 @@
-// import the task definitions from the fixture file "4-tasks.json"
-// iterate over the tasks and create them in the application
-// confirm the task names shown in the List view
-// filter the tasks by part of the name
-// confirm the filtered list of tasks is shown
-
 import { Task } from "../../src/types";
 import { selectors } from "../support/selectors";
 
