977341aaf386201cb3109850766d4c5534b31581
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-14 13:25:37 -0400

    code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import istanbul from "vite-plugin-istanbul";
-import type { UserConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,21 +27,4 @@ export default defineConfig({
       ignored: ["node_modules", "cypress", "cypress/**/*.cy.*", "coverage"],
     },
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      exclude: [
-        "node_modules/",
-        "cypress/",
-        "cypress/**/*.cy.*",
-        "coverage/",
-        "**/*.d.ts",
-        "**/*.test.ts",
-        "**/*.test.tsx",
-      ],
-    },
-  },
-} as UserConfig);
+});
