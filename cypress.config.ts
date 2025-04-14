aef62d3fe6df8f8b8afe4bcc9b0c59ee4e61cf4d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-14 11:47:11 -0400

    feat: add code coverage configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,5 +8,9 @@ export default defineConfig({
     specPattern: "cypress/e2e/**/*.cy.ts",
     video: false,
     screenshotOnRunFailure: false,
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
   build: {
     sourcemap: true,
   },
