23fd585afe64579ffcece4803440abbd92d4fdd3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-17 10:43:14 -0400

    start the task sorting test

---
+++ cypress/e2e/sort-tasks.cy.ts
@@ -0,0 +1,14 @@
+describe("Sort tasks", () => {
+  beforeEach(() => {
+    // visit the app
+    // create 3 tasks with different titles
+    // task B, task A, task C
+  });
+
+  it("sorts by task title", () => {
+    // click on the task title column
+    // confirm the task titles are sorted alphabetically
+    // click the task title column again
+    // confirm the task titles are sorted alphabetically in reverse
+  });
+});
