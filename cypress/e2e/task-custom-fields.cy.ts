90b17b06741845937e0c3a8b754f1b5246689a44
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-25 16:19:46 -0400

    prepare

--- cypress/e2e/task-custom-fields.cy.ts
+++ cypress/e2e/task-custom-fields.cy.ts
@@ -5,11 +5,8 @@ it("adds a task with 2 custom fields", () => {
   // open the create task form
   // enter a title
   // click on the custom fields toggle button
+  // the custom fields element should appear
   // enter a custom field name and value
-  // click on the custom fields toggle button
-  // enter a custom field name and value
-  // click on the create task button
-  // confirm the task is created
+  // click the "Add Field" new custom field button
   // confirm the custom fields are displayed
-  // close the create task form
 });
